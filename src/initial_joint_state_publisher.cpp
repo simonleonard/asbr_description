ea116b77c0f03b955104f6fa2e033229dced8c97
Seyi Afolayan <oafolay2@jh.edu>
2024-07-25 21:54:23 -0400

    took out the initial_joint stuff

--- src/initial_joint_state_publisher.cpp
+++ src/initial_joint_state_publisher.cpp
@@ -9,40 +9,35 @@
 
 using namespace std::chrono_literals;
 
-class InitialJointStatePublisher : public rclcpp::Node
-{
+class InitialJointStatePublisher : public rclcpp::Node {
 public:
-  InitialJointStatePublisher()
-  : Node("initial_joint_state_publisher")
-  {
-    publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
-    RCLCPP_INFO(this->get_logger(), "Initial Joint State Publisher has been started.");
-    publish_timer_ = this->create_wall_timer(
-      500ms, std::bind(&InitialJointStatePublisher::publish_initial_state, this));
-  }
+    InitialJointStatePublisher()
+        : Node("initial_joint_state_publisher") {
+        publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+        publish_initial_state();
+    }
 
 private:
-  void publish_initial_state()
-  {
-    auto message = sensor_msgs::msg::JointState();
-    message.header.stamp = this->now();
-    message.name = {"shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint",
-                    "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"};
-    message.position = {0.0, -1.57, 0.0, -1.57, 0.0, 0.0};
+    void publish_initial_state() {
+        auto message = sensor_msgs::msg::JointState();
+        message.header.stamp = this->now();
+        message.name = {"shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint",
+                        "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"};
+        message.position = {0.0, -1.57, 0.0, -1.57, 0.0, 0.0}; 
+        publisher_->publish(message);
+        RCLCPP_INFO(this->get_logger(), "Published initial joint state.");
 
-    publisher_->publish(message);
-    RCLCPP_INFO(this->get_logger(), "Published initial joint state");
-  }
+        // Stop publishing after setting the initial state
+        rclcpp::shutdown();
+    }
 
-  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher_;
-  rclcpp::TimerBase::SharedPtr publish_timer_;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher_;
 };
 
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<InitialJointStatePublisher>();
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<InitialJointStatePublisher>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
