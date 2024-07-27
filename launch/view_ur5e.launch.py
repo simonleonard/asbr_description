cbfcaf270ab0da6e3782fd8bc9754e38efcb938e
Seyi Afolayan <oafolay2@jh.edu>
2024-07-27 00:37:36 -0400

    added models from assignment2

--- launch/view_ur5e.launch.py
+++ launch/view_ur5e.launch.py
@@ -33,6 +33,8 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
@@ -152,10 +154,26 @@ def generate_launch_description():
         arguments=["-d", rviz_config_file],
     )
 
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([FindPackageShare('gazebo_ros'), '/launch/gazebo.launch.py']),
+    )
+    
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-topic', 'robot_description',
+            '-entity', 'ur5e_robot',
+        ],
+        output='screen'
+    )
+
     nodes_to_start = [
+        gazebo,
         joint_state_publisher_node,
         robot_state_publisher_node,
-        rviz_node,
+        spawn_entity,
+        rviz_node
     ]
 
     return LaunchDescription(declared_arguments + nodes_to_start)
