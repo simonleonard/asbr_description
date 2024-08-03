e34b261f3a9b475ef78eabc7b63ea7e25be69517
Seyi R. Afolayan <oafolay2@jh.edu>
2024-08-03 18:39:33 -0400

    added config

--- launch/asbr_real_ur5_launch.py
+++ launch/asbr_real_ur5_launch.py
@@ -25,43 +25,77 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-
 #
-# Author: Denis Stogl
-# Modified to fit our needs. The code can be remapped at runtime for either ur5 or ur5e
+# Authors: Denis Stogl, Modified by Seyi R. Afolayan
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
     # Declare arguments
-    declared_arguments = []
-    declared_arguments.append(
+    declared_arguments = [
+        # UR specific arguments
+        DeclareLaunchArgument(
+            "ur_type",
+            default_value="ur5e",
+            description="Type/series of used UR robot.",
+            choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20", "ur30"],
+        ),
+        DeclareLaunchArgument(
+            "safety_limits",
+            default_value="true",
+            description="Enables the safety limits controller if true.",
+        ),
+        DeclareLaunchArgument(
+            "safety_pos_margin",
+            default_value="0.15",
+            description="The margin to lower and upper limits in the safety controller.",
+        ),
+        DeclareLaunchArgument(
+            "safety_k_position",
+            default_value="20",
+            description="k-position factor in the safety controller.",
+        ),
+        # General arguments
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="asbr_description",
+            description="Description package with robot URDF/XACRO files. Usually the argument "
+                        "is not set, it enables use of a custom description.",
+        ),
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="asbr.urdf.xacro",
+            description="URDF/XACRO description file with the robot.",
+        ),
+        DeclareLaunchArgument(
+            "tf_prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for "
+                        "multi-robot setup. If changed then also joint names in the controllers' configuration "
+                        "have to be updated.",
+        ),
         DeclareLaunchArgument(
             "robot_ip",
-            default_value="172.22.22.2",  
-            description=" The IP address by which the robot can be reached.",
-        )
-    )
-    declared_arguments.append(
+            default_value="172.22.22.2",
+            description="The IP address of the UR robot."
+        ),
         DeclareLaunchArgument(
             "use_fake_hardware",
             default_value="false",
             description="Start robot with fake hardware mirroring command to its states.",
-        )
-    )
-    declared_arguments.append(
+        ),
         DeclareLaunchArgument(
             "fake_sensor_commands",
             default_value="false",
             description="Enable fake command interfaces for sensors used for simple simulations. "
-            "Used only if 'use_fake_hardware' parameter is true.",
-        )
-    )
-    declared_arguments.append(
+                        "Used only if 'use_fake_hardware' parameter is true.",
+        ),
         DeclareLaunchArgument(
             "initial_joint_controller",
             default_value="scaled_joint_trajectory_controller",
@@ -72,43 +106,96 @@ def generate_launch_description():
                 "forward_velocity_controller",
                 "forward_position_controller",
             ],
-        )
-    )
-    declared_arguments.append(
+        ),
         DeclareLaunchArgument(
             "activate_joint_controller",
             default_value="true",
             description="Activate loaded joint controller.",
-        )
-    )
-
-    declared_arguments.append(
+        ),
         DeclareLaunchArgument(
             "headless_mode",
             default_value="true",
-            description="Enable headless mode for robot control"
+            description="Enable headless mode for robot control",
         )
-    )
+    ]
 
     # Initialize Arguments
+    ur_type = LaunchConfiguration("ur_type")
+    safety_limits = LaunchConfiguration("safety_limits")
+    safety_pos_margin = LaunchConfiguration("safety_pos_margin")
+    safety_k_position = LaunchConfiguration("safety_k_position")
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+    tf_prefix = LaunchConfiguration("tf_prefix")
     robot_ip = LaunchConfiguration("robot_ip")
     use_fake_hardware = LaunchConfiguration("use_fake_hardware")
     fake_sensor_commands = LaunchConfiguration("fake_sensor_commands")
     initial_joint_controller = LaunchConfiguration("initial_joint_controller")
     activate_joint_controller = LaunchConfiguration("activate_joint_controller")
-    headless_mode = LaunchConfiguration("headless_mode") # Suggested by Dr. Leonard
+    headless_mode = LaunchConfiguration("headless_mode")
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
+            " ",
+            "safety_limits:=", safety_limits,
+            " ",
+            "safety_pos_margin:=", safety_pos_margin,
+            " ",
+            "safety_k_position:=", safety_k_position,
+            " ",
+            "name:=", "ur",
+            " ",
+            "ur_type:=", ur_type,
+            " ",
+            "tf_prefix:=", tf_prefix,
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), "rviz", "ur5e.rviz"]
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+    )
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
 
     base_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([FindPackageShare("ur_robot_driver"), "/launch/ur_control.launch.py"]),
         launch_arguments={
-            "ur_type": "ur5",
+            "ur_type": ur_type,
             "robot_ip": robot_ip,
             "use_fake_hardware": use_fake_hardware,
             "fake_sensor_commands": fake_sensor_commands,
             "initial_joint_controller": initial_joint_controller,
             "activate_joint_controller": activate_joint_controller,
             "headless_mode": headless_mode,
+            "robot_description": robot_description_content,
         }.items(),
     )
 
-    return LaunchDescription(declared_arguments + [base_launch])
+    nodes_to_start = [
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+        rviz_node,
+        base_launch
+    ]
+
+    return LaunchDescription(declared_arguments + nodes_to_start)
