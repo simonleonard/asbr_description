118cb3054760bfa5bf2f7e603b0eed36855b07ab
Seyi R. Afolayan <oafolay2@jh.edu>
2024-08-03 12:38:13 -0400

    Created a control launch file

--- launch/control_launch.py
+++ launch/control_launch.py
@@ -1,53 +1,14 @@
-# Copyright (c) 2021 PickNik, Inc.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in the
-#      documentation and/or other materials provided with the distribution.
-#
-#    * Neither the name of the {copyright_holder} nor the names of its
-#      contributors may be used to endorse or promote products derived from
-#      this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Denis Stogl
-
-import os 
+import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import(
-    DeclareLaunchArgument,
-    IncludeLaunchDescription,
-    OpaqueFunction,
-    RegisterEventHandler,
-)
-
-from launch.conditions import IfCondition, UnlessCondition
-from launch.event_handlers import OnProcessExit
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
-
-# Creating the launch function 
+# Creating the launch function
 def launch_setup(context, *args, **kwargs):
     # Initialize Arguments
     ur_type = LaunchConfiguration("ur_type")
@@ -61,15 +22,16 @@ def launch_setup(context, *args, **kwargs):
     description_package = LaunchConfiguration("description_package")
     description_file = LaunchConfiguration("description_file")
     prefix = LaunchConfiguration("prefix")
+    start_joint_controller = LaunchConfiguration("start_joint_controller")
     initial_joint_controller = LaunchConfiguration("initial_joint_controller")
     launch_rviz = LaunchConfiguration("launch_rviz")
 
-    #**** Get the controller configurations and the rviz configuration from the share folder *****#
+    # Get the controller configurations and the RViz configuration from the share folder
     initial_joint_controllers = PathJoinSubstitution(
         [FindPackageShare(runtime_config_package), "config", controllers_file]
     )
     rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare(description_package), "rviz", "view_robot.rviz"]
+        [FindPackageShare("asbr_description"), "rviz", "ur5e.rviz"]
     )
 
     robot_description_content = Command(
@@ -78,39 +40,30 @@ def launch_setup(context, *args, **kwargs):
             " ",
             PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
             " ",
-            "safety_limits:=",
-            safety_limits,
+            "safety_limits:=", safety_limits,
             " ",
-            "safety_pos_margin:=",
-            safety_pos_margin,
+            "safety_pos_margin:=", safety_pos_margin,
             " ",
-            "safety_k_position:=",
-            safety_k_position,
+            "safety_k_position:=", safety_k_position,
             " ",
-            "name:=",
-            "ur",
+            "name:=ur",
             " ",
-            "ur_type:=",
-            ur_type,
+            "ur_type:=", ur_type,
             " ",
-            "prefix:=",
-            prefix, " ",
-            "simulation_controllers:=",
-            initial_joint_controllers,
+            "prefix:=", prefix,
+            " ",
+            "simulation_controllers:=", initial_joint_controllers,
         ]
     )
     robot_description = {"robot_description": robot_description_content}
 
-    joint_state_publisher_node = Node(
-        package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui",
-    )
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
-        parameters=[robot_description],
+        parameters=[{"use_sim_time":True}, robot_description],
     )
+
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
@@ -130,19 +83,18 @@ def launch_setup(context, *args, **kwargs):
         launch_arguments={
             "robot_ip": "172.22.22.2",
             "ur_type": ur_type,
-            "launch_rviz": launch_rviz,
+            "launch_rviz": "true",
             "headless_mode": "true",
-            "initial_joint_controller": initial_joint_controller,
+            "initial_joint_controller": "joint_trajectory_controller",
             "runtime_config_package": runtime_config_package,
         }.items()
     )
 
     nodes_to_start = [
-        joint_state_publisher_node,
+       # joint_state_publisher_node,
         robot_state_publisher_node,
-        rviz_node,
         tf_gripper_node,
-        ur_bringup_launch
+        ur_bringup_launch,
     ]
 
     return nodes_to_start
@@ -185,8 +137,8 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "runtime_config_package",
             default_value="ur_robot_driver",
-            description='Package with the controller\'s configuration in "config" folder'
-            'Usually the argument is not set, it enables use of a custom setup.',
+            description='Package with the controller\'s configuration in "config" folder. '
+                        'Usually the argument is not set, it enables use of a custom setup.',
         )
     )
 
@@ -200,15 +152,15 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_package",
-            default_value="asbr_description",
+            default_value="ur_description",
             description="Description package with robot URDF/XACRO files. Usually the argument "
-            "is not set, it enables use of a custom description.",
+                        "is not set, it enables use of a custom description.",
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_file",
-            default_value="asbr.urdf.xacro",
+            default_value="ur.urdf.xacro",
             description="URDF/XACRO description file with the robot.",
         )
     )
@@ -217,8 +169,16 @@ def generate_launch_description():
             "prefix",
             default_value='""',
             description="Prefix of the joint names, useful for "
-            "multi-robot setup. If changed than also joint names in the controllers' configuration "
-            "have to be updated.",
+                        "multi-robot setup. If changed then also joint names in the controllers' configuration "
+                        "have to be updated.",
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "start_joint_controller",
+            default_value="true",
+            description="Robot controller to start",
         )
     )
 
@@ -239,5 +199,3 @@ def generate_launch_description():
     )
 
     return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
-
-
